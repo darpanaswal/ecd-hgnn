0ad8a3bb775e5b1608ed8f5dcfd1d293736ff354
darpanaswal <darpanaswal@gmail.com>
2025-05-23 00:39:17 +0530

    f1 EarlyStopping Reverted

--- task/GraphPredictionTask.py
+++ task/GraphPredictionTask.py
@@ -70,115 +70,115 @@ def collate_fn(batch):
 
 class GraphPredictionTask(BaseTask):
 
-def __init__(self, args, logger, rgnn, manifold):
-    if args.is_regression:
-        super(GraphPredictionTask, self).__init__(args, logger, criterion='min')
-    else:
-        super(GraphPredictionTask, self).__init__(args, logger, criterion='max')
-    self.hyperbolic = False if args.select_manifold == "euclidean" else True
-    self.rgnn = rgnn
-    self.manifold = manifold
-
-def forward(self, model, sample, loss_function):
-    mask = sample['mask'].cuda()         # shape (B,1)
-    scores = model(sample['node'].cuda().float(),
-                sample['adj_mat'].cuda().long(),
-                sample['weight'].cuda().float(),
-                mask)
-
-    if self.args.is_regression:
-        target = sample['label'][:, self.args.prop_idx].float().cuda()
-        scores_renorm = (scores.view(-1) * self.args.std[self.args.prop_idx]
-                                    + self.args.mean[self.args.prop_idx])
-        loss = loss_function(scores_renorm, target)
-    else:
-        target = sample['label'][:, self.args.prop_idx].long().cuda()
-        loss   = loss_function(scores, target)
-
-    # -------------- return target so the caller can use it ----------
-    return scores, loss, target
-
-def run_gnn(self):
-    train_loader, dev_loader, test_loader = self.load_data()
-
-    task_model = GraphPrediction(self.args, self.logger, self.rgnn, self.manifold).cuda()
-    if getattr(self.args, 'world_suze', 1) > 1:
-        model = nn.parallel.DistributedDataParallel(task_model,
-                                                device_ids=[self.args.device_id],
-                                                output_device=self.args.device_id)
-    else:
-        model = task_model
-    if self.args.is_regression:
-        loss_function = nn.MSELoss(reduction='sum')
-    else:
-        loss_function = nn.CrossEntropyLoss(reduction='sum')
-
-    optimizer, lr_scheduler, hyperbolic_optimizer, hyperbolic_lr_scheduler = \
-                            set_up_optimizer_scheduler(self.hyperbolic, self.args, model)
-    
-    for epoch in range(self.args.max_epochs):
-        self.reset_epoch_stats(epoch, 'train')
-        model.train()
-        for i, sample in enumerate(train_loader):
-            model.zero_grad()
-            scores, loss, target = self.forward(model, sample, loss_function)
-            loss.backward(retain_graph=False)
-
-            if self.args.grad_clip > 0.0:
-                th.nn.utils.clip_grad_norm_(model.parameters(), self.args.grad_clip)
-
-            optimizer.step()
-            if self.hyperbolic and len(self.args.hyp_vars) != 0:
-                hyperbolic_optimizer.step()
-            if self.args.is_regression and self.args.metric == "mae":
-                loss = th.sqrt(loss)
-            self.update_epoch_stats(loss, scores, target,
-                    is_regression=self.args.is_regression)
-            if i % 400 ==0:
-                self.report_epoch_stats()
-        
-        train_acc, train_loss, train_auc, train_prec, train_rec, train_f1 = self.report_epoch_stats()
-        dev_acc, dev_loss, dev_auc, dev_prec, dev_rec, dev_f1 = self.evaluate(epoch, dev_loader, 'dev', model, loss_function)
-        test_acc, test_loss, test_auc, test_prec, test_rec, test_f1 = self.evaluate(epoch, test_loader, 'test', model, loss_function)
-        self.logger.info(f"Epoch {epoch} dev_auc: {dev_auc:.5f}  test_auc: {test_auc:.5f}  dev_f1: {dev_f1:.5f}  test_f1: {test_f1:.5f}")
-
-        # Pass extra info
-        if self.args.is_regression:
-            stop = not self.early_stop.step(
-                dev_loss, test_loss, epoch,
-                train_acc=train_acc, train_auc=train_auc, dev_auc=dev_auc, test_auc=test_auc
-            )
+    def __init__(self, args, logger, rgnn, manifold):
+        if args.is_regression:
+            super(GraphPredictionTask, self).__init__(args, logger, criterion='min')
         else:
-            stop = not self.early_stop.step(
-                dev_acc, test_acc, epoch,
-                train_acc=train_acc, train_auc=train_auc, dev_auc=dev_auc, test_auc=test_auc
-            )
-        if stop:
-            break
+            super(GraphPredictionTask, self).__init__(args, logger, criterion='max')
+        self.hyperbolic = False if args.select_manifold == "euclidean" else True
+        self.rgnn = rgnn
+        self.manifold = manifold
 
-        lr_scheduler.step()
-        if self.hyperbolic and len(self.args.hyp_vars) != 0:
-            hyperbolic_lr_scheduler.step()
-        th.cuda.empty_cache()
-    self.report_best()
+    def forward(self, model, sample, loss_function):
+        mask = sample['mask'].cuda()         # shape (B,1)
+        scores = model(sample['node'].cuda().float(),
+                    sample['adj_mat'].cuda().long(),
+                    sample['weight'].cuda().float(),
+                    mask)
 
-def evaluate(self, epoch, data_loader, prefix, model, loss_function):
-    model.eval()
-    with th.no_grad():
-        self.reset_epoch_stats(epoch, prefix)
-        for i, sample in enumerate(data_loader):
-            scores, loss, target = self.forward(model, sample, loss_function)
-            if self.args.is_regression and self.args.metric == "mae":
-                loss = th.sqrt(loss)
-            self.update_epoch_stats(loss, scores, target,
-                    is_regression=self.args.is_regression)
-        accuracy, loss, roc_auc, precision, recall, f1 = self.report_epoch_stats()
-    if self.args.is_regression and self.args.metric == "rmse":
-        loss = np.sqrt(loss)
-    return accuracy, loss, roc_auc, precision, recall, f1
+        if self.args.is_regression:
+            target = sample['label'][:, self.args.prop_idx].float().cuda()
+            scores_renorm = (scores.view(-1) * self.args.std[self.args.prop_idx]
+                                        + self.args.mean[self.args.prop_idx])
+            loss = loss_function(scores_renorm, target)
+        else:
+            target = sample['label'][:, self.args.prop_idx].long().cuda()
+            loss   = loss_function(scores, target)
 
-def load_data(self):
-    if self.args.task == 'synthetic':
-        return self.load_dataset(SyntheticDataset, collate_fn)
-    else:
-        return self.load_dataset(GraphDataset, collate_fn)	
+        # -------------- return target so the caller can use it ----------
+        return scores, loss, target
+
+    def run_gnn(self):
+        train_loader, dev_loader, test_loader = self.load_data()
+
+        task_model = GraphPrediction(self.args, self.logger, self.rgnn, self.manifold).cuda()
+        if getattr(self.args, 'world_suze', 1) > 1:
+            model = nn.parallel.DistributedDataParallel(task_model,
+                                                    device_ids=[self.args.device_id],
+                                                    output_device=self.args.device_id)
+        else:
+            model = task_model
+        if self.args.is_regression:
+            loss_function = nn.MSELoss(reduction='sum')
+        else:
+            loss_function = nn.CrossEntropyLoss(reduction='sum')
+
+        optimizer, lr_scheduler, hyperbolic_optimizer, hyperbolic_lr_scheduler = \
+                                set_up_optimizer_scheduler(self.hyperbolic, self.args, model)
+        
+        for epoch in range(self.args.max_epochs):
+            self.reset_epoch_stats(epoch, 'train')
+            model.train()
+            for i, sample in enumerate(train_loader):
+                model.zero_grad()
+                scores, loss, target = self.forward(model, sample, loss_function)
+                loss.backward(retain_graph=False)
+
+                if self.args.grad_clip > 0.0:
+                    th.nn.utils.clip_grad_norm_(model.parameters(), self.args.grad_clip)
+
+                optimizer.step()
+                if self.hyperbolic and len(self.args.hyp_vars) != 0:
+                    hyperbolic_optimizer.step()
+                if self.args.is_regression and self.args.metric == "mae":
+                    loss = th.sqrt(loss)
+                self.update_epoch_stats(loss, scores, target,
+                        is_regression=self.args.is_regression)
+                if i % 400 ==0:
+                    self.report_epoch_stats()
+            
+            train_acc, train_loss, train_auc, train_prec, train_rec, train_f1 = self.report_epoch_stats()
+            dev_acc, dev_loss, dev_auc, dev_prec, dev_rec, dev_f1 = self.evaluate(epoch, dev_loader, 'dev', model, loss_function)
+            test_acc, test_loss, test_auc, test_prec, test_rec, test_f1 = self.evaluate(epoch, test_loader, 'test', model, loss_function)
+            self.logger.info(f"Epoch {epoch} dev_auc: {dev_auc:.5f}  test_auc: {test_auc:.5f}  dev_f1: {dev_f1:.5f}  test_f1: {test_f1:.5f}")
+
+            # Pass extra info
+            if self.args.is_regression:
+                stop = not self.early_stop.step(
+                    dev_loss, test_loss, epoch,
+                    train_acc=train_acc, train_auc=train_auc, dev_auc=dev_auc, test_auc=test_auc
+                )
+            else:
+                stop = not self.early_stop.step(
+                    dev_acc, test_acc, epoch,
+                    train_acc=train_acc, train_auc=train_auc, dev_auc=dev_auc, test_auc=test_auc
+                )
+            if stop:
+                break
+
+            lr_scheduler.step()
+            if self.hyperbolic and len(self.args.hyp_vars) != 0:
+                hyperbolic_lr_scheduler.step()
+            th.cuda.empty_cache()
+        self.report_best()
+
+    def evaluate(self, epoch, data_loader, prefix, model, loss_function):
+        model.eval()
+        with th.no_grad():
+            self.reset_epoch_stats(epoch, prefix)
+            for i, sample in enumerate(data_loader):
+                scores, loss, target = self.forward(model, sample, loss_function)
+                if self.args.is_regression and self.args.metric == "mae":
+                    loss = th.sqrt(loss)
+                self.update_epoch_stats(loss, scores, target,
+                        is_regression=self.args.is_regression)
+            accuracy, loss, roc_auc, precision, recall, f1 = self.report_epoch_stats()
+        if self.args.is_regression and self.args.metric == "rmse":
+            loss = np.sqrt(loss)
+        return accuracy, loss, roc_auc, precision, recall, f1
+
+    def load_data(self):
+        if self.args.task == 'synthetic':
+            return self.load_dataset(SyntheticDataset, collate_fn)
+        else:
+            return self.load_dataset(GraphDataset, collate_fn)	
