ae9e001bca508a7083cbefda5a82e3567ec57ea9
darpan.aswal@centralesupelec.fr <aswalda@ruche02.cluster>
2025-08-18 18:02:20 +0200

    POS embeddings

--- slurm.sh
+++ slurm.sh
@@ -20,4 +20,4 @@ echo "Allocated GPU info:"
 nvidia-smi
 
 # Run training
-python main.py --task ecd --select_manifold euclidean --compute_roc_auc
+python main.py --task ecd --select_manifold poincare --compute_roc_auc --use_class_weights
